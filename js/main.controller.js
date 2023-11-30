fc1c03248b8f488fb00ecb9f4945b7420aa11e62
onexsus <boriskho@gmail.com>
2023-11-30 22:16:01 +0200

    render gallery resizecanvas onimgselect

--- js/main.controller.js
+++ js/main.controller.js
@@ -7,34 +7,42 @@ let gCtx;
 
 function onInit() {
   renderGallery()
-  // gElCanvas = document.getElementById("canvas");
-  // gCtx = gElCanvas.getContext("2d");
-  // resizeCanvas();
+  gElCanvas = document.querySelector('canvas')
+  gCtx = gElCanvas.getContext('2d')
+
+  resizeCanvas();
+  window.addEventListener('resize', resizeCanvas)
 }
+
+
+
+
+// Gallery
+function renderGallery(){
+  const elGalllery=document.querySelector('.gallery-continer')
+  var strHTML=''
+  const gImges=getImges()
+  gImges.forEach((obj)=>{
+    strHTML+=`<img src="${obj.url}" onclick="onSelectImg(this)"/>`
+  })
+  elGalllery.innerHTML=strHTML
+}
+
+
+// Canvas 
 function resizeCanvas() {
   const elContainer = document.querySelector(".canvas-container");
   gElCanvas.width = elContainer.offsetWidth;
   gElCanvas.height = elContainer.offsetHeight;
 }
 
-function renderGallery(){
-  const elGalllery=document.querySelector('.gallery-continer')
-  var strHTML=''
-  const gImges=getImges()
-  // gImges.forEach((obj)=>{
-  //   strHTML+=`<img src="${obj.url}" onclick="onSelectImg(this)"/>`
-  //   console.log(obj.url)
-  // })
-  for(let i=0;i<gImges.length;i++){
-    strHTML+=`<img src="${gImges[i].url}" onclick="onSelectImg(this)"/>`
-    console.log(gImges[i].url)
-  }
-  console.log(strHTML)
-  elGalllery.innerHTML=strHTML
-}
-
 function onSelectImg(elImg) {
+  const elGallleryPage=document.querySelector('.meme-gallery-page')
+  elGallleryPage.classList.add('hide')
+  const elMemegen =document.querySelector('.meme-editor-page')
+  elMemegen.classList.remove('hidden')
   coverCanvasWithImg(elImg)
+
 }
 
 function coverCanvasWithImg(elImg) {
