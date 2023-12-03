aef2236508fa0bf8e4ff6fb84c91e5b42f06f95f
onexsus <boriskho@gmail.com>
2023-12-03 20:37:47 +0200

    fix render

--- js/main.controller.js
+++ js/main.controller.js
@@ -75,6 +75,7 @@ function resizeCanvas() {
   const elContainer = document.querySelector(".canvas-container");
   gElCanvas.width = elContainer.offsetWidth;
   gElCanvas.height = elContainer.offsetHeight;
+  renderMeme()
 }
 
 function onSelectImg(imgUrl) {
